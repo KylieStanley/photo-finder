f3fd702a1612e2df11229c50d67fe275adeaea99
Kylie Stanley <knstanley11@gmail.com>
2019-02-25 12:29:19 -0700

    Render divs so background image shows dynamically

---
+++ src/components/Photo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="photo"
+    v-bind:style="{ 'background-image': `url(${image})` }"
+  ></div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: String
+  }
+}
+</script>
+
+<style scoped>
+.photo {
+  border-radius: 10px;
+  border: 3px solid gray;
+  height: 250px;
+  margin: 10px;
+  transition: 0.3s;
+  width: 250px;
+}
+
+.photo:hover {
+  transform: scale(1.1);
+}
+</style>
