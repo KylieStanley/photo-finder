1d39874ef4c33b699c018528398c2090d8b3d564
Kylie Stanley <knstanley11@gmail.com>
2019-02-26 22:39:23 -0700

    Edit server file

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+// Imports.
 const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
-app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
-const port = process.env.PORT || 80
-app.listen(port)
+const history = require('connect-history-api-fallback')
+const app = express()
+// Express server.
+const staticFileMiddleware = express.static(__dirname)
+app.use(staticFileMiddleware)
+app.use(
+  history({
+    disableDotRule: true,
+    verbose: true
+  })
+)
+app.use(staticFileMiddleware)
+const port = 5555
+app.listen(port, () => {
+  console.log('App listening on port ${5555}!')
+})
